ed97bc384fdd43e1ca4b8c4043758882052bbbb2
CharlotteDM <carolinemularczyk@gmail.com>
2024-12-10 13:27:10 +0100

    removing comments

--- code.R
+++ code.R
@@ -18,25 +18,25 @@ movies <- read.csv("christmas_movies.csv", stringsAsFactors = FALSE)
 top10_movies <- movies[order(-movies$imdb_rating), ][1:10, ]
 
 
+#plot - top 10 Christmas Films
 
 ggplot(top10_movies, aes(x = reorder(title, imdb_rating), y = imdb_rating)) +
-  geom_bar(stat = "identity", fill = "#2E8B57", color = "#8B0000", width = 0.7) +  # Zielony i czerwony
-  coord_flip() +  # Obrót wykresu
+  geom_bar(stat = "identity", fill = "#2E8B57", color = "#8B0000", width = 0.7) +  
+  coord_flip() + 
   labs(
-    title = "Top 10 Highest Rated Christmas Movies",  # Tytuł wykresu w języku angielskim
-    x = "Movie Title",  # Etykieta osi X po angielsku
-    y = "IMDb Rating"  # Etykieta osi Y po angielsku
+    title = "Top 10 Highest Rated Christmas Movies",  
+    x = "Movie Title",  
+    y = "IMDb Rating"  
   ) +
-  theme_minimal(base_family = "Comic Sans MS") +  # Lekko zabawny styl
+  theme_minimal(base_family = "Comic Sans MS") + 
   theme(
-    plot.title = element_text(hjust = 0.5, size = 18, color = "#8B0000", face = "bold"),  # Tytuł na środku, kolor i pogrubienie
-    axis.text.x = element_text(color = "#8B0000"),  # Kolor tytułu osi X na czerwono
-    axis.text.y = element_text(color = "#8B0000"),  # Kolor tytułu osi Y na czerwono
-    axis.title.x = element_text(color = "#8B0000", face = "bold"),  # Kolor i pogrubienie tytułu osi X
-    axis.title.y = element_text(color = "#8B0000", face = "bold")   # Kolor i pogrubienie tytułu osi Y
+    plot.title = element_text(hjust = 0.5, size = 18, color = "#8B0000", face = "bold"), 
+    axis.text.x = element_text(color = "#8B0000"), 
+    axis.text.y = element_text(color = "#8B0000"),  
+    axis.title.x = element_text(color = "#8B0000", face = "bold"),  
+    axis.title.y = element_text(color = "#8B0000", face = "bold")   
   ) +
-  # Dodanie ocen na końcu słupków
-  geom_text(aes(label = round(imdb_rating, 1)), hjust = -0.3, color = "#8B0000", size = 5)  # Ocenę IMDb na końcu słupków
+  geom_text(aes(label = round(imdb_rating, 1)), hjust = -0.3, color = "#8B0000", size = 5)  
 
 
 
