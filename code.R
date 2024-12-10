c203cc7dc2c7575edd63f594339b5e0b3234552d
CharlotteDM <carolinemularczyk@gmail.com>
2024-12-10 13:01:58 +0100

    top 10 films

--- code.R
+++ code.R
@@ -11,3 +11,14 @@ setwd(path)
 
 
 movies <- read.csv("christmas_movies.csv", stringsAsFactors = FALSE)
+
+#top 10 films
+top10_movies <- movies[order(-movies$imdb_rating), ][1:10, ]
+
+ggplot(top10_movies, aes(x = reorder(title, imdb_rating), y = imdb_rating)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  coord_flip() +  # Obrócenie wykresu
+  labs(title = "Top 10 Najwyżej Ocenianych Filmów",
+       x = "Tytuł Filmu",
+       y = "Ocena IMDb") +
+  theme_minimal()
